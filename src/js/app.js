0b871ede1f5c624eb5b9a616fe7ac641c5554fb0
Alaya95 <rozetta.95@mail.ru>
2022-10-26 04:04:56 +0300

    added a function to open the menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
-import * as flsFunctions from './modules/functions.js';
+import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+const burgerBtn = document.querySelector("#burger-btn");
+const burgerDrop = document.querySelector("#burger-dropdown");
+const dropdownClass = "dropdown";
+
+burgerBtn.addEventListener("click", () => {
+  burgerDrop.classList.contains(dropdownClass)
+    ? burgerDrop.classList.remove(dropdownClass)
+    : burgerDrop.classList.add(dropdownClass);
+});
